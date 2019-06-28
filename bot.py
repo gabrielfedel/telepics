5a448f73c5b6ec36f72cea18a88d55a0705f732d
Gabriel Fedel <gabriel.fedel@esss.se>
2019-06-28 13:26:04 +0200

    Add camonitor
    Now with message /camonitor <PV> is possible to monitor a PV, receiving
    new message for each change

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import configparser
 
 from telegram.ext import Updater, CommandHandler
-from epics import caget
+from epics import caget, PV
 
 
 class BotEPICS():
     def __init__(self):
-        pass
+        self.pvs_dict = {}
 
     def cmd_caget (self, bot, update):
         pv_name = update["message"]["text"].split(" ")[1]
@@ -17,6 +17,17 @@ class BotEPICS():
     def hello(self, bot, update):
         update.message.reply_text(
             'Hello {}'.format(update.message.from_user.first_name))
+
+    def cmd_camonitor(self, bot, update):
+        pv_name = update["message"]["text"].split(" ")[1]
+        if pv_name not in self.pvs_dict.keys():
+            self.pvs_dict[pv_name] = PV(pv_name)
+            self.pvs_dict[pv_name].add_callback(
+                lambda *args, **kw: self.update_callback(update, *args, **kw))
+
+    def update_callback(self, update=None, pvname=None, value=None, char_value=None, **kw):
+        update.message.reply_text(
+            'PV {} Value {}'.format(pvname, value))
 
 
 if __name__ == '__main__':
@@ -31,6 +42,7 @@ if __name__ == '__main__':
 
     updater.dispatcher.add_handler(CommandHandler('hello', bot.hello))
     updater.dispatcher.add_handler(CommandHandler('caget', bot.cmd_caget))
+    updater.dispatcher.add_handler(CommandHandler('camonitor', bot.cmd_camonitor))
 
     updater.start_polling()
     updater.idle()
