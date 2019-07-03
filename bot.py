a562f7d709956490343a5a97e0e6339b0b828ea1
Gabriel Fedel <gabriel.fedel@esss.se>
2019-07-03 16:55:18 +0200

    Add cacond command
    cacond allow to monitor a PV with a specific condition, using python
    operators.
    
    example:
    /cacond PV < 1
    
    This command will result that the user will be notified only when PV < 1

--- bot.py
+++ bot.py
@@ -21,13 +21,37 @@ class BotEPICS():
     def cmd_camonitor(self, bot, update):
         pv_name = update["message"]["text"].split(" ")[1]
         if pv_name not in self.pvs_dict.keys():
-            self.pvs_dict[pv_name] = PV(pv_name)
-            self.pvs_dict[pv_name].add_callback(
+            self.pvs_dict[pv_name] = {}
+            self.pvs_dict[pv_name]['PV'] = PV(pv_name)
+            self.pvs_dict[pv_name]['PV'].add_callback(
                 lambda *args, **kw: self.update_callback(update, *args, **kw))
 
     def update_callback(self, update=None, pvname=None, value=None, char_value=None, **kw):
-        update.message.reply_text(
-            'PV {} Value {}'.format(pvname, value))
+        # there is no conditition
+        if 'cond' not in self.pvs_dict[pvname].keys() :
+            update.message.reply_text(
+                'PV {} Value {}'.format(pvname, value))
+        else:
+            cond = self.pvs_dict[pvname]['cond']
+            if eval(str(value)+cond):
+                update.message.reply_text(
+                    'PV {} Value {}'.format(pvname, value))
+
+    def cmd_cacond(self, bot, update):
+        msg_cntn = update["message"]["text"].split(" ")[1:]
+        pv_name = msg_cntn[0]
+        if len(msg_cntn) > 2:
+            cond = " ".join(msg_cntn[1:])
+        else:
+            cond = msg_cntn[1]
+
+        if pv_name not in self.pvs_dict.keys():
+            self.pvs_dict[pv_name] = {}
+            self.pvs_dict[pv_name]['PV'] = PV(pv_name)
+            self.pvs_dict[pv_name]['PV'].add_callback(
+                lambda *args, **kw: self.update_callback(update, *args, **kw))
+        # add or update condition
+        self.pvs_dict[pv_name]['cond'] = cond
 
 
 if __name__ == '__main__':
@@ -43,6 +67,7 @@ if __name__ == '__main__':
     updater.dispatcher.add_handler(CommandHandler('hello', bot.hello))
     updater.dispatcher.add_handler(CommandHandler('caget', bot.cmd_caget))
     updater.dispatcher.add_handler(CommandHandler('camonitor', bot.cmd_camonitor))
+    updater.dispatcher.add_handler(CommandHandler('cacond', bot.cmd_cacond))
 
     updater.start_polling()
     updater.idle()
